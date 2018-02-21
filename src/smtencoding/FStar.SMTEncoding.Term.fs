bfc6e3df7e734fb0ecbbc335c2f4100e0e25a65a
A L Manning <asher@zenprotocol.com>
2018-02-21 13:52:14 +0200

    build passing, but still no SMT reasoning about strlen

--- src/smtencoding/FStar.SMTEncoding.Term.fs
+++ src/smtencoding/FStar.SMTEncoding.Term.fs
@@ -44,7 +44,7 @@ let rec strSort x = match x with
   | Bool_sort  -> "Bool"
   | Int_sort  -> "Int"
   | Term_sort -> "Term"
-  | String_sort -> "FString"
+  | String_sort -> "String"
   | Fuel_sort -> "Fuel"
   | BitVec_sort n -> format1 "(_ BitVec %s)" (string_of_int n)
   | Array(s1, s2) -> format2 "(Array %s %s)" (strSort s1) (strSort s2)
@@ -84,6 +84,7 @@ type op =
   | BvUext of Prims.int
   | NatToBv of Prims.int // need to explicitly define the size of the bitvector
   | BvToNat
+  | StrLen
   | ITE
   | Var of string //Op corresponding to a user/encoding-defined uninterpreted function
 
@@ -100,6 +101,7 @@ type qop =
 //de Bruijn representation of terms in locally nameless style
 type term' =
   | Integer    of string //unbounded mathematical integers
+  | StringConstant of string
   | BoundV     of int
   | FreeV      of fv
   | App        of op  * list<term> //ops are always fully applied; we're in a first-order theory
@@ -171,6 +173,7 @@ let fv_of_term = function
     | _ -> failwith "impossible"
 let rec freevars t = match t.tm with
   | Integer _
+  | StringConstant _
   | BoundV _ -> []
   | FreeV fv -> [fv]
   | App(_, tms) -> List.collect freevars tms
@@ -228,6 +231,7 @@ let op_to_string = function
   | BvToNat -> "bv2int"
   | BvUext n -> format1 "(_ zero_extend %s)" (string_of_int n)
   | NatToBv n -> format1 "(_ int2bv %s)" (string_of_int n)
+  | StrLen -> "str.len"
   | Var s -> s
 
 let weightToSmt = function
@@ -236,6 +240,7 @@ let weightToSmt = function
 
 let rec hash_of_term' t = match t with
   | Integer i ->  i
+  | StringConstant s -> s
   | BoundV i  -> "@"^string_of_int i
   | FreeV x   -> fst x ^ ":" ^ strSort (snd x) //Question: Why is the sort part of the hash?
   | App(op, tms) -> "("^(op_to_string op)^(List.map hash_of_term tms |> String.concat " ")^")"
@@ -273,6 +278,7 @@ let isInjective s =
 let mk t r = {tm=t; freevars=BU.mk_ref None; rng=r}
 let mkTrue  r       = mk (App(TrueOp, [])) r
 let mkFalse r       = mk (App(FalseOp, [])) r
+let mkStringConstant s r = mk (StringConstant s) r
 let mkInteger i  r  = mk (Integer (ensure_decimal i)) r
 let mkInteger' i r  = mkInteger (string_of_int i) r
 let mkBoundV i r    = mk (BoundV i) r
@@ -309,6 +315,7 @@ let mkImp (t1, t2) r = match t1.tm, t2.tm with
   | _ -> mkApp'(Imp, [t1; t2]) r
 
 let mk_bin_op op (t1,t2) r = mkApp'(op, [t1;t2]) r
+let mkStrLen t r = mkApp'(StrLen, [t]) r
 let mkMinus t r = mkApp'(Minus, [t]) r
 let mkNatToBv sz t r = mkApp'(NatToBv sz, [t]) r
 let mkBvUext sz t r = mkApp'(BvUext sz, [t]) r
@@ -470,6 +477,7 @@ let abstr fvs t = //fvs is a subset of the free vars of t; the result closes ove
     | _ ->
       begin match t.tm with
         | Integer _
+        | StringConstant _
         | BoundV _ -> t
         | FreeV x ->
           begin match index_of x with
@@ -494,6 +502,7 @@ let inst tms t =
   let n = List.length tms in //instantiate the first n BoundV's with tms, in order
   let rec aux shift t = match t.tm with
     | Integer _
+    | StringConstant _
     | FreeV _ -> t
     | BoundV i ->
       if 0 <= i - shift && i - shift < n
@@ -662,6 +671,7 @@ let termToSmt
         let aux = aux (depth + 1) in
         match t.tm with
         | Integer i     -> i
+        | StringConstant s -> BU.format1 "\"%s\"" s
         | BoundV i ->
           List.nth names i |> fst
         | FreeV x -> fst x
@@ -798,10 +808,7 @@ and declsToSmt        z3options decls = List.map (declToSmt z3options) decls |> 
 
 and mkPrelude z3options =
   let basic = z3options ^
-                "(declare-sort FString)\n\
-                (declare-fun FString_constr_id (FString) Int)\n\
-                \n\
-                (declare-sort Term)\n\
+                "(declare-sort Term)\n\
                 (declare-fun Term_constr_id (Term) Int)\n\
                 (declare-datatypes () ((Fuel \n\
                                         (ZFuel) \n\
@@ -850,7 +857,7 @@ and mkPrelude z3options =
                 (assert (forall ((x Int) (y Int)) (! (= (_div x y) (div x y)) :pattern ((_div x y)))))\n\
                 (assert (forall ((x Int) (y Int)) (! (= (_mod x y) (mod x y)) :pattern ((_mod x y)))))"
    in
-   let constrs : constructors = [("FString_const", ["FString_const_proj_0", Int_sort, true], String_sort, 0, true);
+   let constrs : constructors = [//("FString_const", ["FString_const_proj_0", Int_sort, true], String_sort, 0, true);
                                  ("Tm_type",  [], Term_sort, 2, true);
                                  ("Tm_arrow", [("Tm_arrow_id", Int_sort, true)],  Term_sort, 3, false);
                                  ("Tm_unit",  [], Term_sort, 6, true);
@@ -926,6 +933,23 @@ let unboxTerm sort t = match sort with
   | String_sort -> unboxString t
   | BitVec_sort sz -> unboxBitVec sz t
   | _ -> raise Impos
+
+
+let rec print_smt_term (t:term) :string = match t.tm with
+  | Integer n               -> BU.format1 "(Integer %s)" n
+  | StringConstant s        -> BU.format1 "(StringConstant %s)" s
+  | BoundV  n               -> BU.format1 "(BoundV %s)" (BU.string_of_int n)
+  | FreeV  fv               -> BU.format1 "(FreeV %s)" (fst fv)
+  | App (op, l)             -> BU.format2 "(%s %s)" (op_to_string op) (print_smt_term_list l)
+  | Labeled(t, r1, r2)      -> BU.format2 "(Labeled '%s' %s)" r1 (print_smt_term t)
+  | LblPos(t, s)            -> BU.format2 "(LblPos %s %s)" s (print_smt_term t)
+  | Quant (qop, l, _, _, t) -> BU.format3 "(%s %s %s)" (qop_to_string qop) (print_smt_term_list_list l) (print_smt_term t)
+  | Let (es, body) -> BU.format2 "(let %s %s)" (print_smt_term_list es) (print_smt_term body)
+
+and print_smt_term_list (l:list<term>) :string = List.map print_smt_term l |> String.concat " "
+
+and print_smt_term_list_list (l:list<list<term>>) :string =
+    List.fold_left (fun s l -> (s ^ "; [ " ^ (print_smt_term_list l) ^ " ] ")) "" l
 
 let getBoxedInteger (t:term) =
   match t.tm with
