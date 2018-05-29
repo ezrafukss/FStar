516b46bea972d5acab8424676bc76c6f8a3ee9b9
A L Manning <asher@zenprotocol.com>
2018-05-29 16:59:38 +0300

    Fix module initialisation bug in FStar.Options.fs

--- src/basic/FStar.Options.fs
+++ src/basic/FStar.Options.fs
@@ -1082,7 +1082,7 @@ let resettable_specs = all_specs |> List.filter (fun (_, x, _, _) -> resettable 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 let display_usage () = display_usage_aux (specs())
 
-let fstar_bin_directory = Util.get_exec_dir ()
+// let fstar_bin_directory = Util.get_exec_dir ()
 
 exception File_argument of string
 
@@ -1140,15 +1140,15 @@ let include_path () =
   if get_no_default_includes() then
     get_include()
   else
-    let lib_paths =
-        match FStar.Util.expand_environment_variable "FSTAR_LIB" with
-        | None ->
-          let fstar_home = fstar_bin_directory ^ "/.."  in
-          let defs = universe_include_path_base_dirs in
-          defs |> List.map (fun x -> fstar_home ^ x) |> List.filter file_exists
-        | Some s -> [s]
-    in
-    lib_paths @ get_include() @ [ "." ]
+    //let lib_paths =
+    //    match FStar.Util.expand_environment_variable "FSTAR_LIB" with
+    //    | None ->
+    //      let fstar_home = fstar_bin_directory ^ "/.."  in
+    //      let defs = universe_include_path_base_dirs in
+    //      defs |> List.map (fun x -> fstar_home ^ x) |> List.filter file_exists
+    //    | Some s -> [s]
+    //in
+    get_include() @ [ "." ] //lib_paths @ get_include() @ [ "." ]
 
 let find_file =
   let file_map = FStar.Util.smap_create 100 in
@@ -1392,3 +1392,4 @@ let should_extract m =
         (match get_extract_namespace (), get_extract_module() with
         | [], [] -> true //neither is set
         | _ -> should_extract_namespace m || should_extract_module m)
+
